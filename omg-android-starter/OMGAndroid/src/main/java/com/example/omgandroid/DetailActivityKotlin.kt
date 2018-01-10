2c364c510f6cb407a242901013447c4e09e33ee0
audace-at-dmm <audace@dmmhehe.com>
2018-01-10 11:54:13 +0200

    Kotlin class running

---
+++ omg-android-starter/OMGAndroid/src/main/java/com/example/omgandroid/DetailActivityKotlin.kt
@@ -0,0 +1,15 @@
+package com.example.omgandroid
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * Created by audace-at-dmm on 1/10/18.
+ */
+class DetailActivityKotlin : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        System.out.println("This is what we are doing so far, and we are loving it")
+    }
+}
